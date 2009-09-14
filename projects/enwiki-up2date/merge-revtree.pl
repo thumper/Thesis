d44221538e742802b08d3d874781fd8e60dd59e2
B. Thomas Adler <thumper@ubuntu.(none)>
2009-09-13 21:52:52 -0700

    Code for merging in wikipedia-0.7 files.

---
+++ projects/enwiki-up2date/merge-revtree.pl
@@ -0,0 +1,131 @@
+#!/usr/bin/perl
+
+# Scans the revision tree of already-downloaded revisions,
+# figures out the pageid/revid,
+# and blindly appends to the end of the existing single-file version.
+# Another program will take care of removing duplicates
+# and sorting revisions by timestamp.
+
+use constant SRCDIR => "./wikipedia-0.7/unpack/";
+use constant OUTDIR => "./ensplit-20080103/";
+
+use constant WIKIDB => "DBI:mysql:database=wikidb-thumper:host=localhost";
+use constant DBUSER => "wikiuser";
+use constant DBPASS => "wikiword";
+
+use File::Find;
+use IO::Zlib;
+use Data::Dumper;
+use File::Path qw(mkpath);
+use DBD::mysql;
+use DBI;
+
+
+local $last_pageid = undef;
+local $last_tmp = undef;
+local $last_cleanup = undef;
+
+local $dbh = DBI->connect(WIKIDB, DBUSER, DBPASS);
+die "Bad db: ".$dbh->errstr if !defined $dbh;
+local $sth_rev = $dbh->prepare("SELECT rev_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision WHERE rev_id = ?");
+
+find({ wanted => \&wanted, no_chdir=>1 }, SRCDIR);
+$last_cleanup->() if defined $last_cleanup;
+exit(0);
+
+sub wanted {
+    my $file = $File::Find::name;
+    next if -d $file;
+
+    my ($pageid, $revid);
+    if ($file =~ m#/(\d+)_(\d+)\.gz$#) {
+	$pageid = $1;
+	$revid = $2;
+	die "Bad pageid: [$pageid]" if length($pageid) != 12;
+    } else {
+	die "Unable to parse filename: $file\n";
+	return;
+    }
+
+    $sth_rev->execute($revid) || die $dbh->errstr;
+    my $row = $sth_rev->fetchrow_hashref();
+    die "No meta data for p=$pageid,r=$revid" if !defined $row;
+
+    my $fh = IO::Zlib->new();
+    $fh->open($file, "rb") || die "open: $!";
+    my $text = join('', $fh->getlines());
+    $fh->close();
+
+    xmlEscape($text);
+    foreach my $key (keys %$row) {
+	xmlEscape($row->{$key});
+    }
+    my $ts = $$row{rev_timestamp};
+    if ($ts =~ m/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})$/) {
+	$$row{rev_timestamp}="$1-$2-$3T$4:$5:$6Z";
+    } else {
+	die "Bad timestamp: [$ts]";
+    }
+
+    my $outdir = OUTDIR . join("/", map { substr($pageid, $_, 3) } (0,3,6,9) );
+    mkpath($outdir, { verbose => 1, mode => 0750 });
+     
+    my $destfile = $outdir . "/revisions.txt.gz";
+    my $tmpfile = $outdir . "/.tmp.revs.txt.gz";
+
+    if ($pageid != $last_pageid) {
+	$last_cleanup->() if defined $last_cleanup;
+	$last_pageid = $pageid;
+	$last_tmp = IO::Zlib->new($tmpfile, "wb9") || die "open($tmpfile): $!";
+
+	# need to copy original revs first
+	copy($destfile, $last_tmp);
+	$last_cleanup = sub {
+	    $last_tmp->close();
+	    print "Moving $tmpfile\n\tto $destfile\n";
+	    if (-e $destfile) {
+		unlink($destfile) || die "unlink($destfile): $!";
+	    }
+	    link($tmpfile, $destfile) || die "link($tmpfile, $destfile): $!";
+	    unlink($tmpfile) || die "unlink($tmpfile): $!";
+	};
+    }
+
+    $last_tmp->print("    <revision>\n");
+    $last_tmp->print("      <id>$revid</id>\n");
+    $last_tmp->print("      <timestamp>$$row{rev_timestamp}</timestamp>\n");
+    $last_tmp->print("      <contributor>\n");
+    if ($$row{rev_user_text} =~ m/^\d+\.\d+\.\d+\.\d+$/) {
+	$last_tmp->print("        <ip>$$row{rev_user_text}</ip>\n");
+    } else {
+	$last_tmp->print("        <username>$$row{rev_user_text}</username>\n");
+	$last_tmp->print("        <id>$$row{rev_user}</id>\n");
+    }
+    $last_tmp->print("      </contributor>\n");
+    $last_tmp->print("      <comment>$$row{rev_comment}</comment>\n");
+    $last_tmp->print("      <text xml:space=\"preserve\">$text</text>\n");
+    $last_tmp->print("    </revision>\n");
+
+print "Appended to $tmpfile\n";
+}
+
+sub copy {
+    my ($destfile, $outfh) = @_;
+
+    return if !-e $destfile;
+    my $infh = IO::Zlib->new();
+    $infh->open($tmpfile, "rb") || die "open: $!";
+    while (!$infh->eof()) {
+	my $line = $infh->getline();
+	last if !defined $line;
+	$outfh->print($line);
+    }
+    $infh->close();
+}
+
+sub xmlEscape {
+    $_[0] =~ s/\&/&amp;/g;
+    $_[0] =~ s/\"/&quot;/g;
+    $_[0] =~ s/\</&lt;/g;
+    $_[0] =~ s/\>/&gt;/g;
+}
