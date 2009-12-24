3cb205665a4989002acccdf79366e7e379e2472b
B. Thumper Adler <thumper@redherring.soe.ucsc.edu>
2009-12-24 10:23:07 -0800

    enwiki: modified 'arttree' to move downloaded data to new tree, better naming

--- projects/enwiki-up2date/merge-arttree.pl
+++ projects/enwiki-up2date/merge-arttree.pl
@@ -7,8 +7,8 @@
 # and sorting revisions by timestamp.
 
 use constant SRCDIR1 => "./ensplit2-20080103/";		# split from dump
-use constant SRCDIR2 => "./ensplit-20080103/";		# previously sorted articles
-use constant OUTDIR => "./ensplit3-20080103/";
+use constant SRCDIR2 => "./ensplit-20080103.mmap/";		# previously sorted articles
+use constant OUTDIR => "/big/thumper/ensplit5-20080103/";
 
 use strict;
 use warnings;
@@ -17,8 +17,9 @@ use Data::Dumper;
 use File::Path qw(mkpath);
 
 
+$SIG{CHLD} = 'IGNORE';
 
-find({ wanted => \&mergeFDump, no_chdir=>1 }, SRCDIR1);
+#find({ wanted => \&mergeFDump, no_chdir=>1 }, SRCDIR1);
 find({ wanted => \&mergeFDownload, no_chdir=>1 }, SRCDIR2);
 
 exit(0);
@@ -29,7 +30,7 @@ sub mergeFDownload {
     return if $file !~ m#/revisions\.txt\.gz$#;
 
     my $pageid = $file;
-    $pageid =~ s#^.*\-20080103/##;
+    $pageid =~ s#^.*\-20080103\.mmap/##;
     $pageid =~ s#/revisions\.txt\.gz$##;
     $pageid =~ s#/##g;
     die "Bad pageid: [$pageid]" if length($pageid) != 12;
@@ -44,9 +45,8 @@ sub mergeFDownload {
     chomp($title);
     close(INPUT);
 
-    next if ($title =~ m/<title>(?:Media|Special|Talk|User|User talk|Wikipedia|Wikipedia talk|Image|Image talk|MediaWiki|MediaWiki talk|Template|Template talk|Help|Help talk|Category|Category talk|Portal|Portal talk):/);
-    next if $title eq '';
-    die "Bad title: $file" if $title eq '';
+    return if ($title =~ m/(?:Media|Special|Talk|User|User_talk|Wikipedia|Wikipedia_talk|Image|Image_talk|MediaWiki|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Portal_talk):/);
+    return if $title eq '';
 
     my $outdir = OUTDIR . join("/", map { substr($pageid, $_, 3) } (0,3,6) );
     mkpath($outdir, { verbose => 1, mode => 0750 });
@@ -55,10 +55,11 @@ sub mergeFDownload {
     print OUTPUT "<page>\n<title>", xmlEscape($title), "</title>\n<id>$pageid</id>\n";
     close(OUTPUT);
 
-    rename($file, "$outdir/$pageid.gz") || die "rename($file): $!";
-
-    unlink($meta) || die "unlink($meta): $!";
-
+    return if fork();	# return in parent
+    exec("rsync -avq $file $outdir/$pageid.gz") || die "exec: $!";
+    exit(1);
+    #rename($file, "$outdir/$pageid.gz") || die "rename($file): $!";
+    #unlink($meta) || die "unlink($meta): $!";
 }
 
 sub mergeFDump {
