015943e933ee1c49093b122a8bb5c7094c531c7e
Bo Adler <thumper17@gaston.cse.ucsc.edu>
2011-04-24 22:22:08 -0700

    edit-quality: refinesments on PAN2010 experiment
    + added program to measure performance
    + need to filter revisions so that evalwiki doesn't have to.
      Basically, we don't want to filter the revision immediately
      before, but we do want to filter later revisions.

---
+++ projects/edit-quality/extract-ratingsFrepfile.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use XML::Simple;
+use List::Util qw( min max );
+use lib 'lib';
+use PAN;
+use MediawikiDump;
+
+my ($revids, $repFile) = @ARGV;
+
+my $panrevs = {};
+
+readCSV($revids, [0, 1], sub {
+    my ($revid, $class) = @_;
+    $panrevs->{$revid} = { class => $class };
+});
+
+open(my $editlong, ">perf-editlong.txt") || die "open: $!";
+open(my $textlong, ">perf-textlong.txt") || die "open: $!";
+
+open(my $fh, "<", $repFile) || die "open($repFile): $!";
+while (<$fh>) {
+    next if !m/^VANDALREP/;
+    my @fields = split(' ');
+    next if !exists $panrevs->{$fields[3]};
+    my $revid = $fields[3];
+    my $long = $fields[5];
+    my $class = $panrevs->{$revid}->{class} eq 'vandalism' ? 1 : 0;
+    if ($fields[1] eq 'EditLong') {
+	# convert to probability
+	warn "Crazy long: $long @ $revid" if $long > 1 || $long < -1;
+	$long = min($long, 1.0);
+	$long = max($long, -1.0);
+	$long = (1 + $long) / 2;
+	$long = 1 - $long;
+	print $editlong "$class $long\n";
+	$panrevs->{$revid}->{editlong} = $long;
+    } elsif ($fields[1] eq 'TextLong') {
+	die "Crazy long: $long @ $revid" if $long > 1 || $long < 0;
+	$long = min($long, 1.0);
+	$long = max($long, 0);
+	$long = 1 - $long;
+	print $textlong "$class $long\n";
+	$panrevs->{$revid}->{textlong} = $long;
+    }
+}
+close($fh);
+
+close($editlong);
+close($textlong);
+
+open(my $combo, ">weka-combined.csv") || die "open: $!";
+print $combo "textlong,editlong,class\n";
+foreach my $revid (keys %$panrevs) {
+    my $rev = $panrevs->{$revid};
+    if (exists $rev->{editlong} && exists $rev->{textlong}) {
+	print $combo join(",", $rev->{textlong}, $rev->{editlong},
+	    $rev->{class}) . "\n";
+    }
+    if (!exists $panrevs->{$revid}->{editlong}
+	    && !exists $panrevs->{$revid}->{textlong})
+    {
+	print "Missing both: $revid\n";
+	next;
+    }
+    print "Missing editl: $revid\n" if !exists $panrevs->{$revid}->{editlong};
+    print "Missing textl: $revid\n" if !exists $panrevs->{$revid}->{textlong};
+}
+close($combo);
+
+exit(0);
+
--- projects/edit-quality/extract-revidsFdump.pl
+++ projects/edit-quality/extract-revidsFdump.pl
@@ -24,6 +24,8 @@ my $dump = MediawikiDump->new(sub {
 	# We don't want the "</page>" part...
 	pop @{ $pageData->{header}->{lines}};
     }, sub {
+	print join('', @{ $pagedata->{lastrev}->{lines} })
+	    if $pagedata->{printrevs} > 0;
 	print "  </page>\n" if $pageData->{printpage};
 	$pageData = freshPage();
     }, sub {
@@ -51,32 +53,56 @@ if (0) {
     my $revid = $p->{id}->[0];
 }
     my $revid = undef;
+    my $user = undef;
     foreach ( @{ $revdata->{lines} } ) {
 	if (m/^\s*<id>(\d+)<\/id>/) {
 	    $revid = $1;
-	    last;
 	}
+	if (m/^\s*<username>([^<]+)<\/username>/) {
+	    $user = $1;
+	}
+	if (m/^\s*<ip>([^<]+)<\/ip>/) {
+	    $user = $1;
+	}
+	last if defined $revid && defined $user;
     }
     die "Revision has no id?" if !defined $revid;
+    die "Revision has no user?" if !defined $user;
     $revdata->{revid} = $revid;
+    $revdata->{user} = $user;
     my $panrev = exists $panrevs->{$revid};
     if ($panrev) {
+	# print out <page> tag if this is first time
 	print join('', @{ $pagedata->{header}->{lines} })
 	    if $pagedata->{printpage} == 0;
 	$pagedata->{printpage} = 1;
+	# And print out the lastrev and current rev.
+	# We don't care if they have the same author,
+	# because PAN is asking about this specific rev.
 	print join('', @{ $pagedata->{lastrev}->{lines} })
 	    if defined $pagedata->{lastrev};
-	$pagedata->{lastrev} = undef;
-	# print out the pan rev and the 20 following
-	$pagedata->{printrevs} = 20;
-    }
-    if ($pagedata->{printrevs} > 0) {
 	print join('', @{ $revdata->{lines} });
-	$pagedata->{printrevs}--;
-	confess "Found lastrev?" if defined $pagedata->{lastrev};
-    } else {
-	$pagedata->{lastrev} = $revdata;
+	$pagedata->{lastrev} = undef;
+	# print out the pan rev and the 10 following filtered revs
+	$pagedata->{printrevs} = 10;
+	return;
     }
+    if (!defined $pagedata->{lastrev}) {
+	$pagedata->{lastrev} = $revdata;
+	return;
+    }
+    # filter out revisions if user is the same
+    if ($pagedata->{lastrev}->{user} eq $revdata->{user}) {
+	$pagedata->{lastrev} = $revdata;
+	return;
+    }
+    # print out lastrev if we're wanting that
+    if ($pagedata->{printrevs} > 0) {
+	print join('', @{ $pagedata->{lastrev}->{lines} });
+	$pagedata->{printrevs}--;
+    }
+    # and shift current one into lastrev
+    $pagedata->{lastrev} = $revdata;
 }
 
 
