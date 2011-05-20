3d7b1b41043fc78004eed0313542c68cfb11a959
Bo Adler <thumper17@redherring.soe.ucsc.edu>
2011-05-20 16:32:01 -0700

    edit-quality: fix to use older Date::Manip

--- projects/edit-quality/extract-PAN2010-revids.pl
+++ projects/edit-quality/extract-PAN2010-revids.pl
@@ -4,19 +4,19 @@ use strict;
 use warnings;
 use Text::CSV;
 use Data::Dumper;
-use Date::Manip::Date;
+use Date::Manip;
+use lib 'lib';
+use PAN;
 
 my ($editfile, $goldfile) = @ARGV;
 
 my $edits = {};
 
-my $dateObj = Date::Manip::Date->new();
-
 readCSV($editfile, [0, 3, 5], sub {
     my ($editid, $newrevid, $date) = @_;
-    my $err = $dateObj->parse($date);
-    die "date parse error: $err" if defined $err;
-    my $secs = $date->secs_since_1970_GMT();
+    my $d = ParseDate($date);
+    my $secs = UnixDate($d, "%s");
+    die "Bad time: $secs, $date" if !defined $secs;
     $edits->{$editid} = { revid => $newrevid, 'time' => $secs };
 });
 
@@ -33,20 +33,4 @@ foreach my $editid (keys %$edits) {
 }
 
 exit(0);
-
-sub readCSV {
-    my $file = shift @_;
-    my $fields = shift @_;
-    my $func = shift @_;
-    my $csv = Text::CSV->new({'binary' => 1});
-    open(INPUT, "<".$file) || die "open($file): $!";
-    while (<INPUT>) {
-	chomp;
-	$csv->parse($_) || die "csv parsing error on: " . $csv->error_input
-		."\n" . $csv->error_diag();
-	my @cols = $csv->fields();
-	$func->(map { $cols[$_] } @$fields);
-    }
-    close(INPUT);
-}
 
