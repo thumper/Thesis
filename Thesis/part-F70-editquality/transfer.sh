1f2a542c0750420da8e2c2ffeb848e6e6b31b109
Bo Adler <thumper@alumni.caltech.edu>
2011-07-02 21:46:23 -0700

    thesis: update performance eval of edit-quality

--- Thesis/part-F70-editquality/transfer.sh
+++ Thesis/part-F70-editquality/transfer.sh
@@ -2,3 +2,5 @@
 
 scp thumper17@gaston.cse.ucsc.edu:/raid/thumper/research/Thesis/projects/edit-quality/expt.out-table-editlong-short.tex quality-table-editlong.tex
 scp thumper17@gaston.cse.ucsc.edu:/raid/thumper/research/Thesis/projects/edit-quality/expt.out-table-textlong-short.tex quality-table-textlong.tex
+scp thumper17@gaston.cse.ucsc.edu:/raid/thumper/research/Thesis/projects/edit-quality/expt.out-table-timing.tex quality-table-timing.tex
+
