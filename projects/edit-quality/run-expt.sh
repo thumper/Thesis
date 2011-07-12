feec0151d8ded379a12bca22dd71a55d60f1240b
Bo Adler <thumper17@gaston.cse.ucsc.edu>
2011-07-12 14:36:28 -0700

    editquality: fix expt script to allow running from outside WikiTrust     source tree

--- projects/edit-quality/run-expt.sh
+++ projects/edit-quality/run-expt.sh
@@ -2,6 +2,8 @@
 set -e
 set -x
 
+WORKDIR=/store/thumper/tmp
+OUTPUT=./output
 
 WIKITRUST=/store/thumper/research/WikiTrust
 PANDUMP=/raid/thumper/pan2010dump.7z
@@ -14,50 +16,69 @@ function doexpt {
     echo "**************************************"
     echo "$1"
     echo
-    (cd $WIKITRUST/util ; rm -rf output/blobs)
-    (cd $WIKITRUST/util ; rm -rf output/buckets)
-    (cd $WIKITRUST/util ; rm -rf output/sql)
-    (cd $WIKITRUST/util ; rm -rf output/stats)
-    (cd $WIKITRUST/util ; rm -rf output/user*)
-    (ulimit -v $MAXMEM1 -m $MAXMEM1 -d $MAXMEM1; cd $WIKITRUST/util ; time ./batch_process.py --n_core $CORES1 --cmd_dir ../analysis \
-	  --dir output \
+    (cd $WORKDIR ; rm -rf $OUTPUT/blobs)
+    (cd $WORKDIR ; rm -rf $OUTPUT/buckets)
+    (cd $WORKDIR ; rm -rf $OUTPUT/sql)
+    (cd $WORKDIR ; rm -rf $OUTPUT/stats)
+    (cd $WORKDIR ; rm -rf $OUTPUT/user*)
+    (ulimit -v $MAXMEM1 -m $MAXMEM1 -d $MAXMEM1; cd $WORKDIR ; \
+	time $OUTPUT/cmds/batch_process.py --n_core $CORES1 \
+	  --cmd_dir $OUTPUT/cmds \
+	  --dir $OUTPUT \
 	  $2 \
           --do_compute_stats $PANDUMP)
-    (ulimit -v $MAXMEM2 -m $MAXMEM2 -d $MAXMEM2; cd $WIKITRUST/util ; \
-	time ./batch_process.py --n_core $CORES2 --cmd_dir ../analysis \
-	  --dir output \
+    (ulimit -v $MAXMEM2 -m $MAXMEM2 -d $MAXMEM2; cd $WORKDIR ; \
+	time $OUTPUT/cmds/batch_process.py --n_core $CORES2 \
+	  --cmd_dir $OUTPUT/cmds \
+	  --dir $OUTPUT \
 	  $2 \
 	  --do_sort_stats \
 	  --do_compute_rep $PANDUMP)
-    ./extract-ratingsFrepfile.pl pan2010.csv \
-	$WIKITRUST/util/output/user_reputations.txt
+    (cd $WORKDIR ; $OUTPUT/cmds/extract-ratingsFrepfile.pl pan2010.csv \
+	$OUTPUT/user_reputations.txt )
+    if [ ! -e "perf-editlong.txt" ]; then
+	exit 1
+    fi
     echo EDITLONG
-    ./perf.src/perf < perf-editlong.txt
+    (cd $WORKDIR ; $OUTPUT/cmds/perf < perf-editlong.txt )
     echo TEXTLONG
-    ./perf.src/perf < perf-textlong.txt
-    wc perf-*.txt
-    find $WIKITRUST/util/output/stats -name "*.gz" -exec gunzip -c {} \; | grep triangles > triangles.tmp
+    (cd $WORKDIR ; $OUTPUT/cmds/perf < perf-textlong.txt )
+    (cd $WORKDIR ; wc perf-*.txt )
+    (cd $WORKDIR ; find $OUTPUT/stats -name "*.gz" -exec gunzip -c {} \; | grep triangles > triangles.tmp )
     echo "TOTAL TRIANGLES"
-    awk '{ print $4 }' triangles.tmp | awk -F: '{total+=$1} END{print total}'
+    awk '{ print $4 }' $WORKDIR/triangles.tmp | awk -F: '{total+=$1} END{print total}'
     echo "BAD TRIANGLES"
-    awk '{ print $6 }' triangles.tmp | awk -F: '{total+=$1} END{print total}'
-    rm -f triangles.tmp
+    awk '{ print $6 }' $WORKDIR/triangles.tmp | awk -F: '{total+=$1} END{print total}'
+    rm -f $WORKDIR/perf-editlong.txt
+    rm -f $WORKDIR/perf-textlong.txt
+    rm -f $WORKDIR/triangles.tmp
 }
 
 # First split the wiki once
-(cd $WIKITRUST/util ; rm -rf output*)
-(cd $WIKITRUST/util ; time ./batch_process.py --cmd_dir ../analysis \
-    --dir output --do_split $PANDUMP)
+(cd $WORKDIR ; rm -rf $OUTPUT)
+(cd $WORKDIR ; mkdir -p $OUTPUT/cmds)
+(cp -a $WIKITRUST/analysis/*  $WORKDIR/$OUTPUT/cmds/)
+(rm -f $WORKDIR/$OUTPUT/cmds/*.o)
+(rm -f $WORKDIR/$OUTPUT/cmds/*.ml)
+(rm -f $WORKDIR/$OUTPUT/cmds/*.cm*)
+(rm -f $WORKDIR/$OUTPUT/cmds/*.annot)
+(cp -a ./extract-ratingsFrepfile.pl $WORKDIR/$OUTPUT/cmds/ )
+(cp -a ./perf.src/perf $WORKDIR/$OUTPUT/cmds/ )
+(cp -ar ./lib $WORKDIR/ )
+(cp -ar ./pan2010.csv $WORKDIR/ )
+(cp -a $WIKITRUST/util/batch_process.py  $WORKDIR/$OUTPUT/cmds/)
+(cd $WORKDIR ; time $OUTPUT/cmds/batch_process.py --cmd_dir $OUTPUT/cmds \
+    --dir $OUTPUT --do_split $PANDUMP)
 
 # And then do the experiments
 
 ####################################################################
 
-for diff in {1..8}
+for ed in {1..5}
 do
   for mq in {1..9}
   do
-    for ed in {1..5}
+    for diff in {1..8}
     do
       doexpt "diff=$diff precise match-quality=$mq edist=$ed" "--diff $diff --precise --match_quality $mq --edit_distance $ed"
     done
