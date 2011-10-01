a9ef30293ce541ab0a2a7231d526e9e8084d7132
Bo Adler <thumper@alumni.caltech.edu>
2011-10-01 09:52:18 -0700

    rep-quality: settings for bsi-la

--- projects/reputation-quality/run-expt.sh
+++ projects/reputation-quality/run-expt.sh
@@ -3,17 +3,22 @@ set -e
 set -x
 
 ## Gaston settings
-WORKDIR=/store/thumper/tmp-rep
-WIKITRUST=/store/thumper/research/WikiTrust
-ENDUMP=/raid/dumps/enwiki-20100130-pages-meta-history.xml.bz2
+#WORKDIR=/store/thumper/tmp-rep
+#WIKITRUST=/store/thumper/research/WikiTrust
+#ENDUMP=/raid/dumps/enwiki-20100130-pages-meta-history.xml.bz2
 
 ## Redherring settings
 #WORKDIR=/big/thumper/tmp
 #WIKITRUST=/giant/thumper/research/WikiTrust
 #PANDUMP=/giant/thumper/pan2010dump.7z
 
+## Bsi-la settings
+WORKDIR=/mnt/archive3/tmp-rep
+WIKITRUST=/mnt/archive4/research/WikiTrust
+ENDUMP=/mnt/archive4/enwiki/enwiki-20100130-pages-meta-history.xml.bz2
 
-CORES=6
+
+CORES=2
 OUTPUT=./output
 
 mkdir -p $WORKDIR
