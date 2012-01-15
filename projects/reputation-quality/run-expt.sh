7f2a12aed1bc16d6387b06e1e0d8b943ecca1e44
Bo Adler <thumper@alumni.caltech.edu>
2012-01-15 11:29:42 -0800

    reputation-quality: updated run-expt for gaston enwiki

--- projects/reputation-quality/run-expt.sh
+++ projects/reputation-quality/run-expt.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 
 ## Gaston settings
-WORKDIR=/store/thumper/tmp-rep
+WORKDIR=/raid/thumper/tmp-rep-enwiki
 WIKITRUST=/store/thumper/research/WikiTrust
-ENDUMP=/raid/dumps/itwiki-20090911-pages-meta-history.xml.7z
+ENDUMP=/raid/dumps/enwiki-20100130-pages-meta-history.xml.bz2
 
 ## Redherring settings
 #WORKDIR=/big/thumper/tmp
@@ -13,11 +13,11 @@ ENDUMP=/raid/dumps/itwiki-20090911-pages-meta-history.xml.7z
 #PANDUMP=/giant/thumper/pan2010dump.7z
 
 ## Bsi-la settings
-WORKDIR=/mnt/archive3/tmp-rep
-WIKITRUST=/mnt/archive4/research/WikiTrust
-ENDUMP=/mnt/archive4/enwiki/enwiki-20100130-pages-meta-history.xml.bz2
+#WORKDIR=/mnt/archive3/tmp-rep
+#WIKITRUST=/mnt/archive4/research/WikiTrust
+#ENDUMP=/mnt/archive4/enwiki/enwiki-20100130-pages-meta-history.xml.bz2
 
-CORES=3
+CORES=6
 OUTPUT=./output
 
 mkdir -p $WORKDIR
@@ -33,7 +33,8 @@ cp -ar ./pan2010.csv $WORKDIR/
 cp -a $WIKITRUST/util/batch_process.py  $WORKDIR/$OUTPUT/cmds/
 
 
-(cd $WORKDIR ; time nice python $OUTPUT/cmds/batch_process.py --n_core $CORES --cmd_dir $OUTPUT/cmds \
+(cd $WORKDIR ; time nice python $OUTPUT/cmds/batch_process.py --n_core $CORES \
+  --cmd_dir $OUTPUT/cmds \
   --dir $OUTPUT --do_split --do_compute_stats --do_sort_stats \
   --do_compute_rep --do_compute_trust $ENDUMP)
 
