6efc37ef81f7df6edbfed088e30afaf76ed1bfb7
Bo Adler <thumper@alumni.caltech.edu>
2012-01-29 15:15:37 -0800

    reputation-quality: config for expt

---
+++ projects/reputation-quality/vars.sh
@@ -0,0 +1,20 @@
+
+## Gaston settings
+WORKDIR=/raid/thumper/tmp-rep-enwiki
+WIKITRUST=/store/thumper/research/WikiTrust
+ENDUMP=/store/dumps/enwiki-20100130-pages-meta-history.xml.bz2
+MAXMEM=2000000
+
+## Redherring settings
+#WORKDIR=/big/thumper/tmp
+#WIKITRUST=/giant/thumper/research/WikiTrust
+#PANDUMP=/giant/thumper/pan2010dump.7z
+
+## Bsi-la settings
+#WORKDIR=/mnt/archive3/tmp-rep
+#WIKITRUST=/mnt/archive4/research/WikiTrust
+#ENDUMP=/mnt/archive4/enwiki/enwiki-20100130-pages-meta-history.xml.bz2
+
+CORES=6
+OUTPUT=./output
+
